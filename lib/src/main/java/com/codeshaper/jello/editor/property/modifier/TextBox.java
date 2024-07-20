543187b35b86a4878fdd80c6ab3a497c7ecd9d53
PJ <19332219+CodeShaper13@users.noreply.github.com>
2024-07-20 08:38:59 -0400

    Added javadoc to TextBox annotation

--- lib/src/main/java/com/codeshaper/jello/editor/property/modifier/TextBox.java
+++ lib/src/main/java/com/codeshaper/jello/editor/property/modifier/TextBox.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Makes a string field display as a multi-line textbox instead of a single line
+ * entry field. This annotation has no effect on non-string field.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface TextBox {
