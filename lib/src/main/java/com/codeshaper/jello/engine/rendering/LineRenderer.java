fd939eb44f0e2bc65869d29d9d9580f220c28067
PJ <19332219+CodeShaper13@users.noreply.github.com>
2024-11-29 22:45:54 -0500

    Added icon to LineRenderer component

--- lib/src/main/java/com/codeshaper/jello/engine/rendering/LineRenderer.java
+++ lib/src/main/java/com/codeshaper/jello/engine/rendering/LineRenderer.java
@@ -3,11 +3,13 @@ package com.codeshaper.jello.engine.rendering;
 import org.joml.Vector3d;
 
 import com.codeshaper.jello.editor.property.modifier.MinValue;
+import com.codeshaper.jello.engine.ComponentIcon;
 import com.codeshaper.jello.engine.ComponentName;
 import com.codeshaper.jello.engine.JelloComponent;
 import com.codeshaper.jello.engine.asset.Material;
 
 @ComponentName("Rendering/Line Renderer")
+@ComponentIcon("/editor/componentIcons/lineRenderer.png")
 public final class LineRenderer extends JelloComponent {
 
 	public Vector3d[] points;
